5f07be57ff073824d123ac0f076fdd6cfa26f83c
Cauasb <cauajoaquim.desouzabezerra@gmail.com>
2025-09-03 22:52:56 +0000

    salvar

---
+++ TestesDasAulas.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+float temperatura, umidade;
+unsigned int estoque, estoqueMinimo = 1000;
+
+printf("entre com a temperatura: \n");
+scanf("%f", &temperatura);
+
+printf("Entre com a umidade: \n");
+scanf("%f", &umidade);
+
+printf("entre com o estoque: \n");
+scanf("%u", %estoque);
+
+if( temperatura > 30){
+    printf("Temperatua está alta \n");
+} else{
+    printf("temperatura está dentro dos parâmetros");
+}
+
+if( umidade > 50){
+    printf("Umidade está elevada \n");
+} else {
+    printf(Umidade está dentro dos parâmetros \n");
+}
+
+if( estoque < estoqueMinimo){
+   printf("estoque abaixo do minimo \n);
+{ else {
+    printf("Estoque normal \n")
+
+}
